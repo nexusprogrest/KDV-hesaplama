f93e46f3a8b4984001e1af6425af574503644c55
Arif Yaman <yamanarif05@gmail.com>
2025-09-03 02:36:56 +0300

    Ayrıntı eklendi.

--- src/App.java
+++ src/App.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("değergiriniz:");
+        System.out.println("Ücret değerini giriniz:");
         Scanner input = new Scanner(System.in);
 
-        double x;
+        double x ,a ,b;
+        a = 0.18; b=0.08;
         x = input.nextDouble();
-        double durum = (1000>x) ? (x / 100) * 18 : (x / 100) * 8 ;
-        System.out.println(durum);
+        double KDV_oranı = 1000>x ?  a : b;
+        double KDV_tutarı = (1000>x) ? (x * a) : (x * b) ;
+        double y = x;
+        System.out.println("KDV'siz fiyatı" + x);
+        System.out.println("KDV oranı: " + KDV_oranı);
+        System.out.println("KDV dahil fiyat" + (KDV_tutarı + x));
+        System.out.println("KDV tutarı" + KDV_tutarı);
         input.close();
     }
 }
